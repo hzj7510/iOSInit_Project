69a50e8c6f48dc1ce2469f0e9f3fec10f35a29d9
hzj7510 <hzj7510@163.com>
2017-03-21 17:16:40 +0800

    提交

---
+++ IOSProjectInit/IOSProjectInit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSProjectInit
+//
+//  Created by mac on 17/3/21.
+//  Copyright © 2017年 x5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOSProjectInit/IOSProjectInit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IOSProjectInit
+//
+//  Created by mac on 17/3/21.
+//  Copyright © 2017年 x5. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
